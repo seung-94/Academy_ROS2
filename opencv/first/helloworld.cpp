0b4d203796be5a9e5bbb13c4aa64e1defcab7ddf
han <tanaa1137@gmail.com>
2024-07-23 15:15:56 +0900

    zz

--- opencv/first/helloworld.cpp
+++ opencv/first/helloworld.cpp
@@ -1,9 +1,15 @@
+#include "opencv2/opencv.hpp"
 #include <iostream>
 #include <string>
-#include "opencv2/opencv.hpp"
+
+std::string folder = "/home/ha/Academy_ROS2/opencv/first/";
 
 int main()
 {
-    std::cout<<"hello, world"<<std::endl;
+    std::cout << "hello, world" << std::endl;
+    cv::Mat img;
+    img = cv::imread(folder + "lena.bmp");
+    cv::imshow("image", img);
+    cv::waitKey(1500);
     return 0;
 }
