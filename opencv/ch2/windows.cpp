319c005fc82d347bdad2ad6e0c206332d5ae2732
han <tanaa1137@gmail.com>
2024-07-23 15:38:41 +0900

    aa

---
+++ opencv/ch2/windows.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "opencv2/opencv.hpp"
+using namespace std;
+using namespace cv;
+
+string folder = "/home/ha/Academy_ROS2/opencv/data/";
+
+int main ()
+{
+    Mat img;
+    img = imread(folder + "lena.bmp");
+    namewindow("lena");
+    imshow("lena", img);
+    int key = waitKey(0);
+    cout<< key << endl;
+    Size size;
+    size.height = 100;
+    size.wight = 100;
+    destroyWindow("lena");
+    resizeWindow("lena", size);
+    imshow("lena", img);
+    waitKey(0);
+    return 0;
+}
