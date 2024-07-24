be9ad32449d4be348d383c6cce3a1c019acca19c
han <tanaa1137@gmail.com>
2024-07-24 15:05:38 +0900

    .

--- opencv/ch04/vidio.cpp
+++ opencv/ch04/vidio.cpp
@@ -8,6 +8,48 @@ String folder = "/home/ha/Academy_ROS2/opencv/data/";
 
 int main()
 {
+    Mat frame, doubleFrame, reshapeFrame;
+    VideoCapture cap(folder + "vtest.avi");
+    //VideoCapture cap(0, CAP_V4L2);
+    double fps = cap.get(CAP_PROP_FPS);
+    int delay = cvRound(1000 / fps);
+    Size sz1(cap.get(CAP_PROP_FRAME_WIDTH), cap.get(CAP_PROP_FRAME_HEIGHT));
+    std::vector<int> shape = {sz1.height * 2, sz1.width / 2};
 
+    if (!cap.isOpened())
+    {
+        cerr << "Video open failed " << endl;
+        return -1;
+    }
+    cout << "Video open" << endl;
+    /*
+    while(true){
+        cap >> frame;
+        resize(frame, doubleFrame, sz1 * 2);
+        reshapeFrame = frame.reshape(3, cap.get(CAP_PROP_FRAME_HEIGHT)*2);
+
+        imshow("frame", frame);
+        imshow("doubleFrame", doubleFrame);
+        imshow("reshape", reshapeFrame);
+
+        if (waitKey(delay) == 27)
+            break;
+    }*/
+
+     // frame image show 2
+    while (true)
+    {
+        cap >> frame;
+        resize(frame, doubleFrame, sz1 * 2);
+        reshapeFrame = frame.reshape(3, shape);
+
+        imshow("frame", frame);
+        imshow("doubleframe", doubleFrame);
+        imshow("rashape", reshapeFrame);
+
+        if (waitKey(delay) == 27)
+            break;
+    }
+    cap.release();
     return 0;
 }
