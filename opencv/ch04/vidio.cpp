f0f59b6b9dd07847802a1ef501d2a6d41f83176c
han <tanaa1137@gmail.com>
2024-07-24 14:09:14 +0900

    .

---
+++ opencv/ch04/vidio.cpp
@@ -0,0 +1,13 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+String folder = "/home/ha/Academy_ROS2/opencv/data/";
+
+int main()
+{
+
+    return 0;
+}
