a60e8c9da2a3ee435c40bb75d960ce8c28ca5865
han <tanaa1137@gmail.com>
2024-07-24 12:34:53 +0900

    저장

---
+++ opencv/ch03/mapOp3.cpp
@@ -0,0 +1,26 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+
+String folder = "/home/ha/Academy_ROS2/opencv/data/";
+
+int main()
+{
+    Mat img1 = imread(folder + "lena.bmp");
+    Rect rc(220, 120, 200, 200);
+    Mat img2 = img1(rc);     //roi
+    Mat img3 = img1(rc).clone();       //새로운 이미지
+
+    img2 = ~img2;
+
+    //img1.setTo(Scalar(0,255,255));
+
+    imshow("img1", img1);
+    imshow("img2", img2);
+    imshow("img3", img3);
+
+    waitKey(0);
+
+    return 0;
+}
