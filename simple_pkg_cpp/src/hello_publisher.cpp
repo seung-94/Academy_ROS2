53779f3a8250294f754c0db97c735ef8e56eecfa
han <tanaa1137@gmail.com>
2024-07-30 16:54:28 +0900

    .

--- simple_pkg_cpp/src/hello_publisher.cpp
+++ simple_pkg_cpp/src/hello_publisher.cpp
@@ -1,13 +1,22 @@
 #include "rclcpp/rclcpp.hpp"
+#include <chrono>
 #include <iostream>
 
 using namespace std;
+using namespace std::chrono_literals;
+void printHello();
 
-int main ()
+int main()
 {
     rclcpp::init(0, nullptr);
-    auto node = std::make_shard<rclcpp::Node>("hello_rclcpp");
-    rclcpp::spin(node)
+    auto node = std::make_shared<rclcpp::Node>("hello_rclcpp");
+    auto timer = node->create_wall_timer(1s, printHello);
+    rclcpp::spin(node);
     rclcpp::shutdown();
-    cout << "Hello, World!" <<endl;
+    return 0;
+}
+
+void printHello()
+{
+    cout << "Hello, World!" << endl;
 }
