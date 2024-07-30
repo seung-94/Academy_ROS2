b1b13acf7b9f62f8d90177c86eeacebb876174ba
han <tanaa1137@gmail.com>
2024-07-30 17:26:12 +0900

    .

--- simple_pkg_cpp/src/helloOpenCV.cpp
+++ simple_pkg_cpp/src/helloOpenCV.cpp
@@ -2,11 +2,11 @@
 
 using namespace cv;
 
-int main ()
+int main()
 {
-    Mat img (500, 500, CV_8UC1, Scalar(0));
+    Mat img(500, 500, CV_8UC1, Scalar(0));
     putText(img, "Hello, OpenCV!", Point(50, 250), FONT_HERSHEY_SIMPLEX, 1, Scalar(255), 2);
-    imshow("img",img);
+    imshow("img", img);
     waitKey(0);
     return 0;
 }
