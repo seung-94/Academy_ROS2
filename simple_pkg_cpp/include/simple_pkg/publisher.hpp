f5bc05ee120d81cb7f3f4eae996972e3c23fd08f
han <tanaa1137@gmail.com>
2024-07-31 10:18:30 +0900

    .

---
+++ simple_pkg_cpp/include/simple_pkg/publisher.hpp
@@ -0,0 +1,11 @@
+
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+using namespace std::chrono_literals;
+
+#endif
